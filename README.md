5e4ccc3015c118e4e10e54acb53be7871299b5d3
Lua Govinda Mendes Souza <govinda.souza@mercadolivre.com>
2024-04-02 11:02:01 -0300

    up

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# stripe_fiat_to_crypto_onramp
-stripe_fiat_to_crypto_onramp
+# Integrate with Stripe onramp
+
+https://docs.stripe.com/crypto/integration-recipes
+https://docs.stripe.com/crypto/quotes-api
+
+Build a simple page to use Stripe onramp. Included are some basic
+build and run scripts you can use to start up the application.
+
+## Running the sample
+
+1. Build the server
+
+~~~
+pip3 install -r requirements.txt
+pipenv install -r requirements.txt
+~~~
+
+2. Run the server
+
+~~~
+export FLASK_APP=server.py
+python3 -m flask run --port=4242
+
+export FLASK_APP=server.py
+pipenv run python -m flask run --port=4242
+~~~
+
+3. Build the client app
+
+~~~
+npm install
+~~~
+
+4. Run the client app
+
+~~~
+npm start
+~~~
+
+5. Go to [http://localhost:3000](http://localhost:3000)
---
+++ server.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python3.6
+"""
+Python 3.6 or newer required.
+"""
+import json
+import os
+import stripe
+
+# This is a public sample test API key.
+# Don’t submit any personally identifiable information in requests made with this key.
+# Sign in to see your own test API key embedded in code samples.
+stripe.api_key = 'sk_test_Ho24N7La5CVDtbmpjc377lJI'
+
+from flask import Flask, render_template, jsonify, request
+
+
+app = Flask(__name__, static_folder='public',
+            static_url_path='', template_folder='public')
+
+@app.route('/create-onramp-session', methods=['POST'])
+def create_payment():
+    try:
+        data = json.loads(request.data)
+        # Create an OnrampSession with the order amount and currency
+        onramp_session = stripe.stripe_object.StripeObject().request(
+          "post",
+          "/v1/crypto/onramp_sessions",
+          {
+            "transaction_details": {
+              "destination_currency": data["transaction_details"]["destination_currency"],
+              "destination_exchange_amount": data["transaction_details"]["destination_exchange_amount"],
+              "destination_network": data["transaction_details"]["destination_network"],
+            },
+            "customer_ip_address": request.remote_addr,
+          })
+
+        return jsonify({
+            'clientSecret': onramp_session['client_secret']
+        })
+    except Exception as e:
+        return jsonify(error=str(e)), 403
+if __name__ == '__main__':
+    app.run(port=4242)
